e1dc14f1e653f3931d05014ccd8627a2f093019d
Sarthak <sarthak.grover24.10@gmail.com>
2025-07-10 15:09:59 +0530

    small changes in password_manager
    started making auth.py

--- advanced/password_manager/password_manager.py
+++ advanced/password_manager/password_manager.py
@@ -1,4 +1,11 @@
 import argparse
+import sys
+import bcrypt
+
+# -----paths for the used files------
+
+sys.path.append("../../lib/")
+sys.path.append("./manager/")
 
 # -----functions for application of calls-------
 
@@ -47,15 +54,15 @@ parser = argparse.ArgumentParser(description="A password manager built using pyt
 
 parser.add_argument("-nm", "--newmaster", type=str, nargs=2, metavar=("new_username","masterpassword"), default=None, help="Generates a vault for the given username with the provided master password")
 parser.add_argument("-vo", "--vaultopen", type=str, nargs=2, metavar=("username", "masterpassword"), default=None, help="Open the vault for the user using the master password")
-parser.add_argument("-ad", "--addpassword", type="str", nargs=3, metavar=("tag", "username", "password"), default=None, help="Saves the username and password combo under a tag in the vault")
+parser.add_argument("-ad", "--addpassword", type=str, nargs=3, metavar=("tag", "username", "password"), default=None, help="Saves the username and password combo under a tag in the vault")
 parser.add_argument("-ed", "--edittag", type=str, nargs=3, metavar=("tag", "new/old_username", "newpassword"), default=None, help="Changes the username and password for the given tag")
 parser.add_argument("-dt", "--deletetag", type=str, nargs=1, metavar="tag", default=None, help="Deletes tag and the stored info with tag")
 parser.add_argument("-sh", "--showpassword", type=str, nargs=1, metavar="tag", default=None, help="Shows username and password linked to the tag")
-parser.add_argument("-da", "--deleteall", nargs=0, help="delete all data stored in vault")
-parser.add_argument("-sa", "--showall", nargs=0, help="shows all data stored in vault")
-parser.add_argument("-cv", "--closevault", nargs=0, help="Closes the currently open vault")
+parser.add_argument("-da", "--deleteall", help="delete all data stored in vault")
+parser.add_argument("-sa", "--showall", help="shows all data stored in vault")
+parser.add_argument("-cv", "--closevault", help="Closes the currently open vault")
 parser.add_argument("-dv", "--deletevault", type=str, nargs=2, metavar=("username", "masterpassword"), default="", help="Delete the given vault if password matches")
 
-# ----?????-----
+# -----?????-----
 
 args = parser.parse_args()
