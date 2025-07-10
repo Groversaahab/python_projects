4829390c9b046d5e5d861b65830997c0cda4acf9
Sarthak <sarthak.grover24.10@gmail.com>
2025-07-10 15:48:33 +0530

    Completed auth.py file
    It authenticates password for old user and makes new user

---
+++ advanced/password_manager/manager/auth.py
@@ -0,0 +1,22 @@
+import bcrypt
+import json
+
+def hash_master_password(password: str) -> bytes:
+    password_bytes = password.encode("utf-8")
+    hashed_password = bcrypt.hashpw(password_bytes, bcrypt.gensalt())
+    return hashed_password
+
+def verify_master_password(username: str, password: str) -> bool:
+    with open("username_data.json", "r") as file:
+        loaded_dict = json.load(file)
+        stored_hash = loaded_dict[username]
+        file.close()
+    return bcrypt.checkpw(password.encode("utf-8"), stored_hash)
+
+def save_master_password(username: str, password: str):
+    hashed_password = hash_master_password(password)
+    with open("username_data.json", "w+") as file:
+        loaded_dict = json.load(file)
+        loaded_dict[username] = hashed_password
+        json.dumps(loaded_dict, indent=4)
+        file.close()
